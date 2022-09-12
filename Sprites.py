d1368a9a19b9ad1283ba182e40546a929589560f
16dgreen1 <89630057+16dgreen1@users.noreply.github.com>
2022-09-12 15:24:25 +0100

    made the player point towards the mouse

--- Sprites.py
+++ Sprites.py
@@ -1,5 +1,6 @@
 from Settings import *
 import pygame
+import math
 
 
 class Player(pygame.sprite.Sprite):
@@ -7,18 +8,38 @@ class Player(pygame.sprite.Sprite):
         self.game = game
         self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
-        self.image = pygame.Surface((TILESIZE, TILESIZE))
-        self.image.fill(BLUE)
+        self.image_file = pygame.image.load('images\\Player\\test.png')
+        self.image = self.image_file
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.center = x, y
+        self.rot_angle = 0
         self.dx = 0
         self.dy = 0
 
-    def update(self):
+    def rotate(self):
+        mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+        x = mouse_pos_x - self.rect.x
+        y = mouse_pos_y - self.rect.y
+        if x != 0:
+            self.rot_angle = math.atan(-y/x)
+            self.rot_angle = math.degrees(self.rot_angle)
+        else:
+            if mouse_pos_y < self.rect.y:
+                self.rot_angle = -90
+            else:
+                self.rot_angle = 90
+        if mouse_pos_x < self.rect.x:
+            self.rot_angle += 180
+        self.image = pygame.transform.rotate(self.image_file, self.rot_angle % 360)
+
+    def move(self):
         if self.dx in [1, -1] and self.dy in [1, -1]:
             self.dx *= 0.7
             self.dy *= 0.7
         self.rect.x += self.dx * PLAYER_SPEED
         self.rect.y += self.dy * PLAYER_SPEED
         self.dx, self.dy = 0, 0
+
+    def update(self):
+        self.rotate()
+        self.move()
