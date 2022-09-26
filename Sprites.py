c606639a89f41b5a73d2d576e6fedfae8fd5678a
16dgreen1 <89630057+16dgreen1@users.noreply.github.com>
2022-09-26 15:07:39 +0100

    fixed the rapid spinning when the mouse is too close and when the 180 didn't apply for certain values of x

--- Sprites.py
+++ Sprites.py
@@ -20,8 +20,8 @@ class Player(pygame.sprite.Sprite):
         original_coords = self.rect.center
         # AB = b - a
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
-        x = mouse_pos_x - self.rect.x
-        y = mouse_pos_y - self.rect.y
+        x = mouse_pos_x - self.rect.centerx
+        y = mouse_pos_y - self.rect.centery
         if x != 0:
             # angle between player and mouse
             self.rot_angle = math.degrees(math.atan(-y/x))
@@ -32,8 +32,9 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rot_angle = -90
         # if the mouse is behind the player in terms of x
-        if mouse_pos_x < self.rect.x:
+        if mouse_pos_x < self.rect.centerx:
             self.rot_angle += 180
+        print(self.rot_angle)
         # change the player
         self.image = pygame.transform.rotate(self.image_file, self.rot_angle % 360)
         self.rect = self.image.get_rect()
