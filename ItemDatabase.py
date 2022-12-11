8b0c40dce7c8e5ac8904e1c9c9dd2e81fbcc19d4
16dgreen1 <89630057+16dgreen1@users.noreply.github.com>
2022-12-11 12:23:06 +0000

    updated items database and table

--- ItemDatabase.py
+++ ItemDatabase.py
@@ -2,13 +2,14 @@ import sqlite3
 
 connector = sqlite3.connect("item.db")
 cursor = connector.cursor()
-cursor.execute("""CREATE TABLE items (
-name text,
-damage real,
-firerate real
-spread real
-bullet_speed real
-bullets_at_once integer
-)""")
+# cursor.execute("""CREATE TABLE items (
+# name text,
+# damage real,
+# firerate real,
+# spread real,
+# bullet_speed real,
+# bullets_at_once integer,
+# fire_type text
+# )""")
 connector.commit()
 connector.close()
